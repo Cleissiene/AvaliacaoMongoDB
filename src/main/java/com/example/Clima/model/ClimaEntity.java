911b656d3a93f279a08464deecb183382921bc86
11513288 <@123>
2024-04-19 19:27:09 -0300

    Feat conexao mongo2

--- src/main/java/com/example/Clima/model/ClimaEntity.java
+++ src/main/java/com/example/Clima/model/ClimaEntity.java
@@ -3,7 +3,7 @@ package com.example.Clima.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "clima")
+@Document(collection = "bd-clima")
 public class ClimaEntity {
     @Id
     private String id;
